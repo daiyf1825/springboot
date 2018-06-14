dcef3a2d8f1f43845b0f40c7e7c1664c0d6f6a24
ixiye <ixiye@zldeMacBook-Pro.local>
2018-06-14 17:21:41 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String index(){
+		return "hello world";
+	}
+}
